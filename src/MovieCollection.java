3ce639e4b3e7d190bb13eb6b2cddd70ff99606ed
nasif <nasifafif@gmail.com>
2025-02-25 20:09:13 -0500

    changed searchtitles

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -8,6 +8,8 @@ public class MovieCollection
 {
     private int movieNum;
     private ArrayList<Movie> movies = new ArrayList<Movie>();
+    private ArrayList<String> possibleMovies = new ArrayList<>();
+    private ArrayList<Integer> listOfIndexes = new ArrayList<>();
     public MovieCollection(){}
 
     public void importData(){
@@ -16,21 +18,35 @@ public class MovieCollection
             Scanner scanner = new Scanner(myFile);
             while(scanner.hasNext()){
                 String data = scanner.nextLine();
-                String [] split = data.split(",");
+                String[] split = data.split(",");
                 String title = split[0];
                 String[] cast = split[1].split("\\|");
                 String director = split[2];
                 String overview = split[3];
-                int runtime = Integer.parseInt(split[4]);
-                double userRating = Double.parseDouble(split[5]);
-                Movie movie = new Movie(title,cast,director,overview,runtime,userRating);
-                movies.add(movie);
 
+                int runtime = 0;
+                try {
+                    runtime = Integer.parseInt(split[4]);
+                } catch (NumberFormatException e) {
+                    System.out.print("");
+                }
+
+                double userRating = 0.0;
+                try {
+                    userRating = Double.parseDouble(split[5]);
+                } catch (NumberFormatException e) {
+                    System.out.println();
+                }
+
+                Movie movie = new Movie(title, cast, director, overview, runtime, userRating);
+                movies.add(movie);
             }
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
     }
+
+
     public void menu(){
         System.out.println("Welcome to the movie collection!");
         String menuOption = "";
@@ -56,35 +72,48 @@ public class MovieCollection
         }
 
     }
-    public boolean linearSearchFound(ArrayList<Movie> title, String titleName){
+    public void linearSearchMovie(ArrayList<Movie> title, String titleName){
+
         for(int i = 0; i<title.size(); i++){
-            if(movies.get(i).getTitle().toLowerCase().equals(titleName.toLowerCase())){
+            if(movies.get(i).getTitle().toLowerCase().contains(titleName.toLowerCase())){
                 movieNum = i;
-                return true;
+                listOfIndexes.add(movieNum);
+                possibleMovies.add(movies.get(movieNum).getTitle());
             }
         }
-        return false;
     }
 
 
     public void searchTitles(){
+        int count = 0;
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome! Enter a title of a movie to search the database.");
         String title = scan.nextLine();
-        if(linearSearchFound(movies,title)){
-            System.out.println("Title: " + movies.get(movieNum).getTitle());
-            System.out.println("Cast: " + Arrays.toString(movies.get(movieNum).getCast()));
-            System.out.println("Director: " + movies.get(movieNum).getDirector());
-            System.out.println("Overview: " + movies.get(movieNum).getOverview());
-            System.out.println("Runtime: " + movies.get(movieNum).getRuntime());
-            System.out.println("User rating: " + movies.get(movieNum).getUserRating());
+        linearSearchMovie(movies,title);
+        if(!possibleMovies.isEmpty()){
+            for(int i = 0; i<possibleMovies.size(); i++){
+                count++;
+                System.out.println(count + ":" + possibleMovies.get(i));
+            }
+            System.out.println("Which movie would you like to learn about? (Enter number)");
+            int choice = scan.nextInt()-1;
+            choice = listOfIndexes.get(choice);
+            System.out.println("Title: " + movies.get(choice).getTitle());
+            System.out.println("Cast: " + Arrays.toString(movies.get(choice).getCast()));
+            System.out.println("Director: " + movies.get(choice).getDirector());
+            System.out.println("Overview: " + movies.get(choice).getOverview());
+            System.out.println("Runtime: " + movies.get(choice).getRuntime() + " minutes");
+            System.out.println("User rating: " + movies.get(choice).getUserRating());
         }
         else{
             System.out.println("Movie not found.");
         }
+    }
+
+    public void searchCast(){
+
 
     }
-    public void searchCast(){};
 
 
 }
